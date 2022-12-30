574cb4b4ec12e9f8a6dc3aa41b3076054dabb774
Rasmus Rask <rasse.rask@gmail.com>
2022-12-30 10:07:13 +0200

    added check for char in ft_printf

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -1,41 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rrask <rrask@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/29 13:30:09 by rrask             #+#    #+#             */
-/*   Updated: 2022/12/30 10:05:40 by rrask            ###   ########.fr       */
+/*   Created: 2022/12/30 09:59:43 by rrask             #+#    #+#             */
+/*   Updated: 2022/12/30 10:00:00 by rrask            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-int	handle_c(int counter, char *s)
+void	ft_putchar_fd(char c, int fd)
 {
-	ft_putchar_fd(*s, 1);
-	counter++;
-	return (counter);
-}
-
-int	ft_printf(const char *s, ...)
-{
-	int		i;
-	va_list	args;
-
-	i = 0;
-	va_start(args, s);
-	while (s[i])
-	{
-		if (s[i] == '%')
-		{
-			i++;
-			if (s[i] == 'c')
-				handle_c(i, (char *)s);
-		}
-		i++;
-	}
-	va_end(args);
-	return (1);
+	write(fd, &c, 1);
 }
--- header.h
+++ header.h
@@ -6,7 +6,7 @@
 /*   By: rrask <rrask@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/29 13:36:12 by rrask             #+#    #+#             */
-/*   Updated: 2022/12/29 16:08:03 by rrask            ###   ########.fr       */
+/*   Updated: 2022/12/30 10:01:50 by rrask            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,6 @@
 
 int		ft_printf(const char *s, ...);
 void	ft_putstr_fd(char *s, int fd);
+void	ft_putchar_fd(char c, int fd);
 
 #endif
