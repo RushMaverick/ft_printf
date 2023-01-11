071dc7c6bd513d0a4e3c4c762e0d8cbbfb84f24c
Rasmus Rask <rasse.rask@gmail.com>
2023-01-11 12:37:41 +0200

    Added my own test file in the git version

---
+++ Tester/main.c
@@ -0,0 +1,32 @@
+#include "ft_printf.h"
+#include <limits.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	// int	pr_len;
+	// int x;
+	// int y;
+	// pr_len = 0;
+	// printf("OG char: %c\n", 'A');
+	// ft_printf("My char: %c\n", 'A', 'a');
+	// x = printf("OG: NULL %s NULL ", NULL);
+	// y = ft_printf("My: NULL %s NULL ", NULL);;
+	// printf("OG amount; %d\n", x);
+	// printf("My amount; %d\n", y);
+	// x = printf("OG int and digit: %d\n", INT_MIN);
+	// y = ft_printf("My int and digit: %d\n", INT_MIN);
+	// printf("OG amount; %d\n", x);
+	// printf("My amount; %d\n", y);
+	// printf("OG percent: %%\n");
+	// ft_printf("My percent: %%\n");
+	// printf("OG lowercase hexa: %x\n", 12334123);
+	// ft_printf("My lowercase hexa: %x\n", 12334123);
+	// printf("OG uppercase hexa: %X\n", 12334123);
+	// ft_printf("Me uppercase hexa: %X\n", 12334123);
+	// printf(" %u \n", 0);
+	// ft_printf(" %u \n", 0);
+	// printf("OG v o i d:  %p ", -1);
+	// ft_printf("My v o i d:  %p ", -1);
+	return (0);
+}
